05de31ab27a1280bf58b9c9373964ee0c01b68f3
ryanli002004 <ryanli002004@gmail.com>
2023-08-31 17:25:31 -0400

    fix day 12

--- day12folder/day12.py
+++ day12folder/day12.py
@@ -2,12 +2,12 @@
 print("100 Days of Code QUIZ")
 print()
 print("How many can you answer correctly?")
-ans1 = ("What language are we writing in?")
+ans1 = input("What language are we writing in?")
 if ans1 == "python":
     print("Correct")
 else:
     print("Nope🙈")
-ans2 = input("Which lesson number is this?")
+ans2 = int(input("Which lesson number is this?"))
 if (ans2>12):
     print("We're not quite that far yet")
 elif(ans2==12):
