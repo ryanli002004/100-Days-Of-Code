cb050351df7d20769afc0d5c3fee18eeeb73731d
ryanli002004 <ryanli002004@gmail.com>
2023-09-02 09:27:36 -0400

    fix

--- day59folder/day59.py
+++ day59folder/day59.py
@@ -6,10 +6,10 @@ word4 = "is"
 
 def pani(x):
     if x[0] != x[-1] and len(x) > 1:
-        return "not a pani!"
+        return "not a palindrome!"
     elif x[0] == x[-1] and len(x) > 1:
         return pani(x[1:-1])
     else:
-        return "pani!"
+        return "palindrome!"
 
 print(pani(word1))
